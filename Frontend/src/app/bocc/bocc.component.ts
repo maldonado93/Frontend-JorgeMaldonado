f3c5623fa76995d1afcbb545933b8707465215f2
maldonado93 <jorgemaldonad12@hotmail.com>
2021-07-14 21:48:51 -0500

    service

--- Frontend/src/app/bocc/bocc.component.ts
+++ Frontend/src/app/bocc/bocc.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BoccService } from '../services/bocc.service';
+import {BoccModule} from '../models/bocc/bocc.module'
 
 @Component({
   selector: 'app-bocc',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BoccComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private BoccService: BoccService) { }
+  clientes: BoccModule[];
   ngOnInit(): void {
+    this.BoccService.getClientes().subscribe(
+      clientes => this.clientes = clientes
+    )
   }
+  
 
 }
--- Frontend/src/app/services/bocc.service.ts
+++ Frontend/src/app/services/bocc.service.ts
@@ -7,15 +7,14 @@ import { map } from 'rxjs/operators';
 @Injectable()
 export class BoccService {
 
-  constructor(private http:HttpClient) { }
-  private urlEndPoint = "https://m4b03ywe18.execute-api.us-east-1.amazonaws.com/dev/express/bocc_customers/800220154";
-  private httpHeaders = new HttpHeaders({ "Content-Type": "application/json" });
+  // constructor(private http:HttpClient) { }
+  // private urlEndPoint = "https://m4b03ywe18.execute-api.us-east-1.amazonaws.com/dev/express/bocc_customers/800220154";
+  // private httpHeaders = new HttpHeaders({ "Content-Type": "application/json" });
 
-  getClientes(): Observable<BoccModule[]> {
-    //return of(CLIENTES);
-    return this.http.get(this.urlEndPoint).pipe(
-      map(response => response as BoccModule[])
-    );
-  }
+  // getClientes(): Observable<BoccModule> {
+  //   return this.http.get(this.urlEndPoint).pipe(
+  //     map(response => response as BoccModule)
+  //   );
+  // }
 
 }
